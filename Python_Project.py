57160e9e1b24832c37e5b85209a9bf66ab6bef33
root <root@ip-10-47-174-141.ec2.internal>
2015-06-08 04:18:41 +0000

    Committed by Udacity

---
+++ Python_Project.py
@@ -0,0 +1,232 @@
+title = ['''<h1><b>Lesson 1: Basics of the Web</b></h1>''',
+         '''<h1><b>Lesson 1.2: Getting Started</b></h1>''',
+         '''<h1><b>Lesson 1.3: CSS Code Style</b></h1>''',
+         '''<h1><b>Lesson 2.1: Serious Programming</b></h1>''',
+         '''<h1><b>Lesson 2.3: Functions</b></h1>''',
+         '''<h1><b>Section 2.4-2.5: Flow, Loops, and Debugging</b></h1>''',]
+
+content = ['''<div class="L1">
+    <h3>How The Web Works</h3>
+    <p>The web is a collection of computer the communicate with each other to relay
+    inormation. The computer uses <em>HTTP</em> to ask  <em>servers</em> for a
+    particular webpage or <em>HTML Document</em>.
+      </div>
+    <br>
+      <div class="L1">
+      <h3>HTML</h3>
+    <p><b>Html</b> is described a the "the heart of the web." Any and all webpages would
+    not exist if not for HTML. HTML itself is made of <em>many</em> individual aspects
+    to keep it together. The actual <b>text content</b> is what is seen and read on the
+    webpage. The <b>markup</b> is how it is displayed on the webpage.</p>
+      </div>
+    <br>
+      <div class="L1">
+      <h3>HTML Markup</h3>
+    <p>HTML markup is control by elements known as <em>tags</em> and <em>attributes</em>.
+    <b>Tags</b> are used to alter the appearance of specific words or phrases. All tags
+    are used in the middle of a "less than sign" and a "greater than sign". To make a
+    word <b>bold</b>, you would use the "b" tag and to <em>italize</em>, the "em" tag
+    would be used.<b>Attributes</b> are complementary assets to some tags. An anchor tag,
+    for example, would be accompanied by the attribute "href", while "src" or search
+    would be used for an image or "img" tag.</p>
+      </div>
+    <br>
+      <div class="L1">
+      <h3>Span and Div</h3>
+    <p><b>Span</b> and <b>Div</b> are two classes that are used to contain a specific
+    word or phrase. Div class is a <b>block</b> element, meaning it creates a "box"
+    around the specified word or phrase. Span is an <b>inline</b> element, similar to
+    "a","br","strong",etc.
+       </div>
+     </div>
+     <div class="Lesson">''',
+           '''<div class="L1">
+    <h3>Learning how to learn</h3>
+    <p>Learning how to do anything can be simplified into 4 relatively simple stages.
+    The first stage would be <em>Ignorance</em> or "Stage 0." 0 is fitting for this
+    stage because you know nothing at this stage. Stage 1 is the <em>Awareness</em>
+    stage, which can be achieved by asking a simple question: What is that? Stage 2 is
+    <em>Ability</em> which is just being able to do something at some level of
+    competency. Stage 3 is <em>Fluency</em>, being able to do something in your sleep.
+    </p>
+     </div>
+     <br>
+     <div class="L1">
+    <h3>Leave Mastery to the Masters</h3>
+    <p>Fluency is not the current target, but it is still a goal. The current targets
+    are Ability and Awareness. The reasoning is quite simple: the more you know about
+    something and the more you do that, the quicker and easier it is to <em>master</em>
+    it.</p>
+        </div>
+        <br>
+        <div class="L1">
+    <h3>Understanding the Language</h3>
+    <p><b>HTML</b> and <b>CSS</b> are both languages that can be looked at as two sides
+    of the same coin. HTML and the tags inside of it control the structure of the page.
+    The structure of the page is what tells the browser what the content is and how to
+    display it: text, picture, tag, etc. It's done by connecting several different
+    elements into a tree-like "structure". CSS controls the style of the page, or the
+    way it looks. The definition of language in this sense is a combination of
+    <b>syntax</b> and <b>rules</b>.</p>
+        </div>
+      </div>
+      </div>
+      <div class="Lesson">''',
+           '''<div class="L1">
+        <h3>What not to do</h3>
+        <p>The first lesson in using CSS is to avoid repetition! CSS standards for
+        <b>Cascading Style Sheet</b>, which roughly translates to a sheet that takes
+        the <em>most descriptive</em> rule that it is given and applies that rule to an
+        html document. CSS sort of acts like a home decorator in a sense that it will
+        customize the webpage to the specifications of the client.  </p>
+      </div>
+      <br>
+        <div class="L1">
+        <h3>How CSS works</h3>
+        <p>CSS targets a specific class or tag by using a <b>selector</b>. Using a
+        selector tells the CSS to target all classes or tags with this name and changes
+        them according to the given rules. The rules provided to the CSS are
+        <b>declarations</b> made up of a <b>property</b> and a <b>value</b>. </p>
+      </div>
+      <br>
+        <div class="L1">
+        <h3>How to use CSS</h3>
+        <p>CSS can be used in a number of ways:</p>
+         <ol>
+          <li>Write it in to the initial HTML file through the in the head element,</li>
+          <li>Link it in as a separate file all together,</li> <li>or Write it in the
+          HTML with the corresponding element by inlining it, which is a bad idea, because
+          it requires repetition for similar elements and that breaks Rule No. 1.</li>
+      </ol></div>
+      </div>
+      <div class="Lesson">''',
+           ''' <div class="L1">
+        <h3>Computer Science</h3>
+        <p>Computer science is problem solving, with computer. Specifically, how a
+        computer solves a problem. Computers can do anything, as long as they have the
+        proper <b>program (instructions)</b>, and a <b>precise sequence of steps</b> to
+        tell the computer how to use those instructions. Once a computer receives those
+        instructions, it <b>computes</b> the command and <b>executes</b> the programs.
+        </p>
+      </div>
+      <br>
+        <div class="L1">
+        <h3>Computer Languages</h3>
+        <p>Computers may display natural language, but they cannot run their programs on
+        it. Natural languages have several <b>ambiguities</b>, which would confuse a
+        computer and its programs. Therefore, computers have their own languages to run
+        on known as <b>high-level language</b>. To translate the natural language that
+        humans type into their computers, a computer <em>needs an interpreter program</em>
+        to change the language to one the computer can understand without fault.</p>
+      </div>
+      <br>
+        <div class="L1">
+        <h3>Python Program</h3>
+        <p>The <b>Python Program</b> is a computer program that runs as an interpreter.
+        Python translates instructions given in a specific form known as the <b>Backus
+        Naur Form</b>. The instructions can be given in sentence form: first Noun, then
+        Verb and finally Object. <em>(Sentence = Subject->Verb->Object)</em>. Python
+        runs on a <b>variable</b> system. Variables ,in python, are elements that can be
+        use to assign a name a specific value. The assignemnt statement goes: Name =
+        Expression. 
+        </p>
+      </div>
+      <br>
+        <div class="L1">
+        <h3>Variables</h3>
+        <p>The variables in a python program are a way to give a name a value. To assign
+        a value to a variable means that this variable is now a representation of the
+        expression.</p>
+        <p> The value is assigned using the "=". "=" doesn't mean equal
+        like it does in mathematics: In programming, "=" roughly means, "This value is
+        now represented by this variable." "2+3=5" is an result of the 2 + 3, but "my_variable=5" now means that ,in the program, my_variable has the value of 5. </p> 
+        <p> There are some specifics in dealing with variables: 2+2 and "2"+"2" result
+        in two completely diffent results. 2+2 results in 4, because it's formatted as
+        a math problem and is asking for addition. "2"+"2" asks what happens when you
+        combine "2" and "2", not add them together, so the result is 22.</p>
+      </div>
+      </div>
+        <div class="Lesson">''',
+           '''<div class="L1">
+            <h3>What are Functions</h3>
+        <p><b>Functions</b> are programs given to the computer for it to execute.
+        Functions require an <b>input</b>, meaning you give the function a value and
+        an expression to compute, and the result is an <b>output</b>, or a result of
+        the input.({function[(input),(input)]} would yeild an <em>output</em> using the
+        keyword "return")  </p>
+      </div>
+      <br>
+          <div class="L1">
+            <h3>Making and using Functions</h3>
+      <p>To make or <b>define</b> a function, the code has to start with the keyword
+      "def". The "def" is then followed by a function name and its <b>parameters</b>.
+      The parameters recieve an actual value further down the code, when the is used</p>
+      <p>To use, or <b>call</b> a function is to give the parameters a value and run the
+      function with those values.
+      </p>
+       </div>
+       <br>
+           <div class="L1">
+            <h3>Useful but Tricky</h3>
+      <p>Functions are a great tool because they help <em><b>avoid repititon</b></em>,
+      which is a rule for programmers. Once the function is programmed, it never has to
+      be re-programmed and can be used whenever it's needed.
+      </p>
+      <p>However, functions are still very specific and without the right components,
+      the function is useless. An example is the "return" part of the function. Without
+      it, absolutely nothing happens. The result that shows up says "None". 
+      </p>
+           </div>
+        </div>
+        <div class="Lesson">''',
+           '''</h1>
+          <div class="L1">
+            <h3>IF and OR</h3>
+      <p>The <b>"IF"</b> statement is used to find a Boolean Value of the given expression
+      , or simply wether the expression is <em>True</em> or <em>False</em> within a
+      function. The same can be said for the <b>"OR"</b> statement, except the "OR"
+      statement can be easier to use by condensing the code.</p>
+          </div>
+      <br>
+          <div class="L1">
+            <h3>Debugging</h3>
+      <p>Bugs. Will. Happen. There are 5 ways to approach debugging a program, and
+      anyone will do, it's just based on preference.
+        <ol>
+           <li>Examine the error messages</li>
+           <li>Use an example code as a reference</li>
+           <li>Make sure the examples actually work</li>
+           <li>Test print intermediate results</li>
+           <li>Keep old program attempts for comparison</li>
+        </ol>
+      </p> 
+    </div>
+        </div>
+      </div>
+  </body>
+
+</html>''']
+
+def title_sort(x):
+  if x >= 0:
+      return title[x]
+
+def content_sort(y):
+    if y >= 0:
+        return content[y]
+
+def page_gen(x,y):
+  paragraph_1 = title_sort(x)
+  paragraph_2 = content_sort(y)
+  skeleton = paragraph_1 + '''
+''' + ''' ''' + paragraph_2
+  return skeleton
+
+
+def finalize(x,y):
+    while x <= 5 and y <= 5:
+        print page_gen(x,y)
+        x = x + 1
+        y = y + 1
+        
+print finalize(0,0)
